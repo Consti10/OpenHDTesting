278d38f5e921457077a4290f2e1f87eb057ffde8
ru26kif <constantin.geier@campus.lmu.de>
2020-10-28 19:11:57 +0100

    X

--- my_wfb_tx.sh
+++ my_wfb_tx.sh
@@ -3,5 +3,5 @@
 NICS_LIST=`ls /sys/class/net/ | nice grep -v eth0 | nice grep -v lo | nice grep -v usb | nice grep -v intwifi | nice grep -v wlan | nice grep -v relay | nice grep -v wifihotspot`
 
 echo "Start MY TX $NICS_LIST \n"
-/usr/local/share/cameracontrol/IPCamera/svpcom_wifibroadcast/wfb_tx -k 4 -n 8 -u 6002 -p 60 -K /tmp/tx.key $NICS_LIST
+/usr/local/share/cameracontrol/IPCamera/svpcom_wifibroadcast/wfb_tx -M 4 -k 4 -n 8 -u 6002 -p 60 -K /tmp/tx.key $NICS_LIST
 	
